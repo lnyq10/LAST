3c90a38a29de3c3c0906e8c6ad896c60fb20a847
JI_Niqiu <lnyq10@sjtu.edu.cn>
2018-08-31 19:02:23 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define LIN64
+#include <base/abc/abc.h>
+#include <base/main/main.h>
+
+
+int main() {
+    abc::Abc_Obj_t_ niqiu;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
