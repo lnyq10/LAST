0ef076ab58cbd5efca02dfbe772d828186e382a0
JI_Niqiu <lnyq10@sjtu.edu.cn>
2018-08-31 19:37:55 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#define LIN64
-#include <base/abc/abc.h>
-#include <base/main/main.h>
+#include <abc_api.h>
 
+using namespace abc;
+
+extern ABC_DLL Abc_Ntk_t * Abc_NtkCreateMffc( Abc_Ntk_t * pNtk, Abc_Obj_t * pNode,
+        char * PNodeName);
 
 int main() {
     abc::Abc_Obj_t_ niqiu;
+    auto* origin_ntk = Io_ReadBlif((char*)"./benchmark/ctrl.blif", 1);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
