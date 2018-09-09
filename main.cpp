8821d163a93756c3b7d94cb847cca9e91fa7c09c
JI_Niqiu <lnyq10@sjtu.edu.cn>
2018-09-09 11:23:20 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,12 +3,30 @@
 
 using namespace abc;
 
-extern ABC_DLL Abc_Ntk_t * Abc_NtkCreateMffc( Abc_Ntk_t * pNtk, Abc_Obj_t * pNode,
-        char * PNodeName);
 
 int main() {
-    abc::Abc_Obj_t_ niqiu;
-    auto* origin_ntk = Io_ReadBlif((char*)"./benchmark/ctrl.blif", 1);
+    //abc::Abc_Obj_t_ niqiu;
+    //auto* origin_ntk = Io_ReadBlif((char*)"./benchmark/test.blif", 1);
+    //origin_ntk = Abc_NtkToLogic(origin_ntk);
+
+    //auto Pins = origin_ntk->vPios;
+    //auto Pins_deR1 = *Pins->pArray;
+    //Io_WriteBlifLogic(origin_ntk, (char*)"./FastAccess/out.blif", 0);
+
+    auto Par = (If_Par_t*) malloc(sizeof(If_Par_t));
+    If_ManSetDefaultPars(Par);
+    auto Man = If_ManStart(Par);
+    //auto i = If_ManPerformMappingComb(Man);
+
+
+    free(Par);
+
+
+    auto bar = new If_Obj_t_;
+    bar->EstRefs = 2.3;
+    delete bar;
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
