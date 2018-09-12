bbb51c01fbbf1585cc0ffb144d256e520fc53184
XiangFrank <41998027+XiangFrank@users.noreply.github.com>
2018-09-12 22:10:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,5 +10,15 @@ int main() {
     abc::Abc_Obj_t_ niqiu;
     auto* origin_ntk = Io_ReadBlif((char*)"./benchmark/ctrl.blif", 1);
     std::cout << "Hello, World!" << std::endl;
+    Abc_Frame_t * pAbc;
+    Abc_Start();
+    pAbc = Abc_FrameGetGlobalFrame();
+    char* command1="read_blif ./benchmark/ctrl.blif";
+    char* command2="if -K 4";
+    char* command3="write_blif ./new_mapLUT.blif";
+    Cmd_CommandExecute(pAbc, command1);
+    Cmd_CommandExecute(pAbc, command2);
+    Cmd_CommandExecute(pAbc, command3);
+    Abc_End();
     return 0;
 }
