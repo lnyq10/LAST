17086f1c851a2d127b300b3198437fb6fe02d607
JI_Niqiu <lnyq10@sjtu.edu.cn>
2018-09-24 12:46:27 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <abc_api.h>
+#include <util.h>
 #include <vector>
 
 using namespace abc;
-
-void Abc_NodeMffcConeSuppPrint(Abc_Obj_t* pObj);
+using namespace std;
 
 int main() {
+    //Abc_Obj_t
     /*
     abc::Abc_Obj_t_ niqiu;
     auto* origin_ntk = Io_ReadBlif((char*)"./benchmark/ctrl.blif", 1);
     std::cout << "Hello, World!" << std::endl;
-
     Abc_Frame_t * pAbc;
     Abc_Start();
     pAbc = Abc_FrameGetGlobalFrame();
@@ -24,30 +24,22 @@ int main() {
     Abc_Stop();
     return 0;
     */
-    char* filename = (char*) "./benchmark/div.blif";
-    Abc_Ntk_t* blif_ntk = Io_ReadBlif(filename,1);
-    blif_ntk = Abc_NtkToLogic(blif_ntk);
-    int NodeNum = Abc_NtkNodeNum(blif_ntk);
+    char* filename = (char*) "./benchmark/ectl.blif";
+    //Abc_Ntk_t* blif_ntk = Io_ReadBlif(filename,1);
+    auto blif_ntk = ReadBlif(filename);
 
 
-
-    std::vector<std::vector<Vec_Ptr_t*>> nodeSet_vec;
     Vec_Ptr_t *vCone, *vSupp;
-
 
     int i = 0;
     Abc_Obj_t* pNode;
     Abc_NtkForEachNode(blif_ntk, pNode, i){
-        int num = Abc_NodeMffcSize(pNode);
 
         vCone = Vec_PtrAlloc(100);
         vSupp = Vec_PtrAlloc(100);
-        Abc_NodeDeref_rec(pNode);
-        Abc_NodeMffcConeSupp(pNode, vCone, vSupp);
-        Abc_NodeRef_rec(pNode);
-        std::cout << Vec_PtrSize(vCone) << std::endl;
-        std::cout << "The supp size is " << vSupp->nSize << std::endl;
-        Abc_NodeMffcConeSuppPrint(pNode);
+        cout << "Node name is " << Abc_ObjName(pNode) << endl;
+        cout << "Fanin is " << Abc_ObjName(Abc_ObjFanin(pNode, 1)) << endl;
+        NodeMffcConeSupp(pNode, vCone, vSupp, true);
         Vec_PtrFree(vCone);
         Vec_PtrFree(vSupp);
     }
@@ -56,21 +48,3 @@ int main() {
 }
 
 
-void Abc_NodeMffcConeSuppPrint( Abc_Obj_t * pNode )
-{
-    Vec_Ptr_t * vCone, * vSupp;
-    Abc_Obj_t * pObj;
-    int i;
-    vCone = Vec_PtrAlloc( 100 );
-    vSupp = Vec_PtrAlloc( 100 );
-    Abc_NodeDeref_rec( pNode );
-    Abc_NodeMffcConeSupp( pNode, vCone, vSupp );
-    Abc_NodeRef_rec( pNode );
-    printf( "Node = %6s : Supp = %3d  Cone = %3d  (",
-            Abc_ObjName(pNode), Vec_PtrSize(vSupp), Vec_PtrSize(vCone) );
-    Vec_PtrForEachEntry( Abc_Obj_t *, vCone, pObj, i )
-        printf( " %s", Abc_ObjName(pObj) );
-    printf( " )\n" );
-    Vec_PtrFree( vCone );
-    Vec_PtrFree( vSupp );
-}
