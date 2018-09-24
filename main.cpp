c7a96bd00e737269451f7b4b04cfafad9a15e3b2
JI_Niqiu <lnyq10@sjtu.edu.cn>
2018-09-24 16:08:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,44 +7,38 @@ using namespace abc;
 using namespace std;
 
 int main() {
-    //Abc_Obj_t
-    /*
-    abc::Abc_Obj_t_ niqiu;
-    auto* origin_ntk = Io_ReadBlif((char*)"./benchmark/ctrl.blif", 1);
-    std::cout << "Hello, World!" << std::endl;
-    Abc_Frame_t * pAbc;
-    Abc_Start();
-    pAbc = Abc_FrameGetGlobalFrame();
-    auto read=(char*)"read_blif ./benchmark/ctrl.blif";
-    auto convert=(char*)"if -K 4";
-    auto write=(char*)"write_blif ./Lut_circuit/ctrl.blif";
-    Cmd_CommandExecute(pAbc, read);
-    Cmd_CommandExecute(pAbc, convert);
-    Cmd_CommandExecute(pAbc, write);
-    Abc_Stop();
-    return 0;
-    */
     char* filename = (char*) "./benchmark/ectl.blif";
-    //Abc_Ntk_t* blif_ntk = Io_ReadBlif(filename,1);
     auto blif_ntk = ReadBlif(filename);
 
-
     Vec_Ptr_t *vCone, *vSupp;
+    vector<Abc_Ntk_t*> pNtkSet;
 
     int i = 0;
     Abc_Obj_t* pNode;
     Abc_NtkForEachNode(blif_ntk, pNode, i){
 
-        vCone = Vec_PtrAlloc(100);
-        vSupp = Vec_PtrAlloc(100);
-        cout << "Node name is " << Abc_ObjName(pNode) << endl;
-        cout << "Fanin is " << Abc_ObjName(Abc_ObjFanin(pNode, 1)) << endl;
-        NodeMffcConeSupp(pNode, vCone, vSupp, true);
-        Vec_PtrFree(vCone);
-        Vec_PtrFree(vSupp);
+            vCone = Vec_PtrAlloc(100);
+            vSupp = Vec_PtrAlloc(100);
+            NodeMffcConeSupp(pNode, vCone, vSupp, true);
+
+            if(vCone->nSize >= 4) {
+                auto temp = (Abc_Obj_t *) vCone->pArray[0];
+                Abc_Ntk_t *pNtkNew = Abc_NtkCreateMffc(blif_ntk, temp, Abc_ObjName(pNode));
+                pNtkSet.push_back(pNtkNew);
+            }
+
+            Vec_PtrFree(vCone);
+            Vec_PtrFree(vSupp);
     }
 
+
+    vector<Abc_Ntk_t*>::size_type ix;
+    for(ix = 0; ix != pNtkSet.size(); ix++)
+        Abc_NtkDelete(pNtkSet[ix]);
+
+    Abc_NtkDelete(blif_ntk);
     return 0;
 }
+
 
 
