48f40159c9ce383edce27fc651b240865ce293dd
JI_Niqiu <lnyq10@sjtu.edu.cn>
2018-09-24 12:50:34 +0800

    Add files via upload

--- util/util.cpp
+++ util/util.cpp
@@ -4,7 +4,7 @@
 
 #include <cstdio>
 #include "util.h"
-
+using namespace abc;
 
 void NodeMffcConeSupp(Abc_Obj_t* pNode, Vec_Ptr_t* vCone, Vec_Ptr_t* vSupp, bool print){
     Abc_NodeDeref_rec(pNode);
@@ -26,4 +26,18 @@ Abc_Ntk_t* ReadBlif(char* filename){
     Abc_Ntk_t* blif_ntk = Io_ReadBlif(filename,1);
     blif_ntk = Abc_NtkToLogic(blif_ntk);
     return blif_ntk;
+}
+
+void map_LUT(char* old_filename, char* new_filename){
+    Abc_Frame_t * pAbc;
+    Abc_Start();
+    pAbc = Abc_FrameGetGlobalFrame();
+    char command[1000];
+    sprintf(command,"read_blif %s",old_filename);
+    Cmd_CommandExecute(pAbc, command);
+    char* command2="if -K 4";
+    Cmd_CommandExecute(pAbc, command2);
+    sprintf(command,"write_blif %s",new_filename);
+    Cmd_CommandExecute(pAbc, command);
+    Abc_Stop();
 }
--- util/util.h
+++ util/util.h
@@ -7,12 +7,11 @@
 
 #include "abc_api.h"
 
-using namespace abc;
-
 //void Abc_NodeMffcConeSuppPrint( Abc_Obj_t * pNode );
 void NodeMffcConeSupp(Abc_Obj_t* pNode, Vec_Ptr_t* vCone, Vec_Ptr_t* vSupp, bool print);
+
 Abc_Ntk_t* ReadBlif(char* filename);
 
-
+void map_LUT(char* old_filename, char* new_filename);
 
 #endif //ABC_LAST_UTIL_H
