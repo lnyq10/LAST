af9b3bd45ceaef6cf4048fd082aa1026df9a9cea
XiangFrank <41998027+XiangFrank@users.noreply.github.com>
2018-10-07 22:29:31 +0800

    Update util.h

--- util/util.h
+++ util/util.h
@@ -27,6 +27,34 @@ map<int ,int> getNtkSetInfo(vector<Abc_Ntk_t*> pNtkSet);
 //Home many times the cone of this size appear in pNtkSet
 
 void map_LUT(char* old_filename, char* new_filename);
+class truth_table{
+private:
+    int** truthtable;
+    int row;
+    int column;
+public:
+    void print_truth_table();
+    truth_table(int** truth_table, int row, int column){
+        this->truthtable=truth_table;
+        this->column=column;
+        this->row=row;
+    }
+    ~truth_table(){
+        for(int i=0; i<column; i++){
+            delete[] truthtable[i];
+        }
+        delete[] truthtable;
+    }
+};
 
+int inverse(int num);
+
+void binary(int* array, int size, int num);
+
+void simulate_node(Abc_Ntk_t* LUT_network, Abc_Obj_t* pnode);
+
+int simulation(Abc_Ntk_t* LUT_network, int* bound_set, int* free_set, int bound_size, int free_size, int input_row, int input_column);
+
+truth_table create_truth_table(Abc_Ntk_t* LUT_network, int* bound_set, int* free_set, int num);
 
 #endif //ABC_LAST_UTIL_H
